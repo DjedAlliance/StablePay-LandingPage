7db4fe48f23ae35ac7b18d28789e597d39b7d4b0
Nidhicodes <nidhisingh200418@gmail.com>
2025-01-20 16:06:02 +0000

    Replace placeholder img with SVG logo in Header and Footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,5 +1,6 @@
 import XSocial from '@/assets/social-x.svg'
 import YTSocial from '@/assets/social-youtube.svg'
+import Logo from '@/assets/logo.svg';
 
 export const Footer = () => {
   return (
@@ -8,7 +9,7 @@ export const Footer = () => {
         <div className="container">
           <div className="flex flex-col lg:flex-row lg:items-center gap-5">
             <div className="flex gap-2 items-center lg:flex-1">
-              <img src="/favicon.ico" alt="StablePay Logo" className="h-6 w-6" />
+            <Logo className="h-8 w-8" />
               <div className="font-medium">StablePay</div>
             </div>
             <nav className="flex flex-col lg:flex-row gap-2 lg:gap-4 lg:flex-1 lg:justify-center">
