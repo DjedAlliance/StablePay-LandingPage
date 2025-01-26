17c46a51de052892006a8f98f9ab4e11efe371fb
pavanydg <pavanyermunja@gmail.com>
2025-01-26 21:58:19 +0530

    updated next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'))
+    output: 'export',
+    distDir: 'out',
+    images: {
+        unoptimized: true,
+    },
+    basePath: '/StablePay-LandingPage',
+    webpack(config) {
+        const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'))
 
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
-      // Convert all other *.svg imports to React components
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-        use: ['@svgr/webpack'],
-      }
-    )
+        config.module.rules.push(
+            {
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                resourceQuery: /url/, // *.svg?url
+            },
+            // Convert all other *.svg imports to React components
+            {
+                test: /\.svg$/i,
+                issuer: fileLoaderRule.issuer,
+                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+                use: ['@svgr/webpack'],
+            }
+        )
 
-    fileLoaderRule.exclude = /\.svg$/i
+        fileLoaderRule.exclude = /\.svg$/i
 
-    return config
-  },
+        return config
+    },
 }
 
 export default nextConfig
